a077720c10188b728e6805eae52e3926b83f18cd
melliemuse <melliemuse@gmail.com>
2020-05-31 17:36:26 -0500

    redid user intervnetions model, built get and post requests

--- braineaserAPI/views/__init__.py
+++ braineaserAPI/views/__init__.py
@@ -4,3 +4,4 @@ from .baseline_anxiety_score import BaselineAnxietyScores
 from .register import register_user
 from .register import login_user
 from .intervention import Interventions
+from .userIntervention import UserInterventions
