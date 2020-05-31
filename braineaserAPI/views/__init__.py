bccfd4321ec5e15752370fef0b0ef69fa6e47f6f
melliemuse <melliemuse@gmail.com>
2020-05-31 11:56:48 -0500

    added registration feature

--- braineaserAPI/views/__init__.py
+++ braineaserAPI/views/__init__.py
@@ -1,2 +1,5 @@
 from .client import Clients
 from .user import Users
+from .baseline_anxiety_score import BaselineAnxietyScores
+from .register import register_user
+from .register import login_user
--- braineaserBackEnd/urls.py
+++ braineaserBackEnd/urls.py
@@ -19,7 +19,7 @@ from rest_framework import routers
 from rest_framework.authtoken.views import obtain_auth_token
 from django.contrib.auth.models import User
 from django.contrib import admin
-from braineaserAPI.views import Clients, Users
+from braineaserAPI.views import Clients, Users, register_user, login_user
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'clients', Clients, 'client')
@@ -28,7 +28,8 @@ router.register(r'users', Users, 'user')
 urlpatterns = [
     path('', include(router.urls)),
     path('api-token-auth/', obtain_auth_token),
-    #  path('login', login_user),
+    path('register', register_user),
+    path('login/', login_user),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
