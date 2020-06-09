c6432e3912d5e445863b003e9f7301d628710c42
melliemuse <melliemuse@gmail.com>
2020-06-09 17:30:40 -0500

    fixed error

--- braineaserAPI/views/journal.py
+++ braineaserAPI/views/journal.py
@@ -18,7 +18,8 @@ class JournalSerializer(serializers.HyperlinkedModelSerializer):
             view_name="journal",
             lookup_field="id"
         )
-        fields = ('id', 'client', 'entry', 'prompt')
+        fields = ('id', 'client', 'prompt', 'entry')
+        depth = 1
 
 class Journals(ViewSet):
     def retrieve(self, request, pk=None):
@@ -48,12 +49,12 @@ class Journals(ViewSet):
         user = self.request.query_params.get('user', None)
         
         if user is not None:
-            journal = Journal.objects.filter(client = request.auth.user.client.id)
-            serializer = JournalSerializer(journal, many = True, context={'request': request})
+            journal = Journal.objects.filter(client=request.auth.user.client.id)
+            serializer = JournalSerializer(journal, many=True, context={'request': request})
             return Response(serializer.data)
         else:
             journal = Journal.objects.all()
-            serializer = JournalSerializer(journal, context={'request': request})
+            serializer = JournalSerializer(journal, many=True, context={'request': request})
             return Response(serializer.data)
         
 
